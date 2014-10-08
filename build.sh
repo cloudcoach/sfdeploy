2ba579091da55164e9016f731b3ca1752724bca3
brentgorton <brent.gorton@cloudcoach.com>
2014-10-08 13:22:39 -0600

    Added runAllTests true

--- build.sh
+++ build.sh
@@ -1,12 +1,48 @@
 #!/bin/bash
 set -ev
 
-export sfusername="${devusername}"
-export sfpassword="${devpassword}"
+sfdeploy=".sfdeploy"
+undeploy="false"
+deploy="false"
+runAllTests="true"
+#if[ -e "$sfdeploy" ]
+#then
+	chmod a+x .sfdeploy
+	./.sfdeploy
+#	if["${TRAVIS_PULL_REQUEST}" = "true"]
+#	then
+#		if["${sfbuildpulls}" == "true"]
+#		then
+#			export sfusername="${pullusername}";
+#			export sfpassword="${pullpassword}";
+#			undeploy="true"
+#			deploy="true"
+#		else
+#			exit 1;
+#		fi
+#	else
+#		if["${TRAVIS_BRANCH}" = "dev"]
+#		then
+#			export sfusername="${devusername}";
+#			export sfpassword="${devpassword}";
+#			undeploy="false"
+#			deploy="true"
+#		else
+#			if["${TRAVIS_BRANCH}" = "master"]
+#			then
+#				export sfusername="${pkgusername}";
+#				export sfpassword="${pkgusername}";
+#				undeploy="false"
+#				deploy="true"
+#			else
+#				exit 1;
+#			fi
+#		fi
+#	fi
 
 if [ "${undeploy}" = "true" ]; then
   ant undeploy
 fi
 if [ "${deploy}" = "true" ]; then
   ant deploy
-fi 
+fi
