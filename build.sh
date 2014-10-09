1e07fc124d6427c83b5fd63398a7ab174d034b8b
brentgorton <brent.gorton@cloudcoach.com>
2014-10-09 11:45:49 -0600

    Removed unneeded lines from build script

--- build.sh
+++ build.sh
@@ -1,43 +1,5 @@
 #!/bin/bash
 set -ev
-
-#sfdeploy=".sfdeploy"
-#undeploy="false"
-#deploy="false"
-#if[ -e "$sfdeploy" ]
-#then
-#chmod a+x .sfdeploy
-#	./.sfdeploy
-#	if["${TRAVIS_PULL_REQUEST}" = "true"]
-#	then
-#		if["${sfbuildpulls}" == "true"]
-#		then
-#			export sfusername="${pullusername}";
-#			export sfpassword="${pullpassword}";
-#			undeploy="true"
-#			deploy="true"
-#		else
-#			exit 1;
-#		fi
-#	else
-#		if["${TRAVIS_BRANCH}" = "dev"]
-#		then
-#			export sfusername="${devusername}";
-#			export sfpassword="${devpassword}";
-#			undeploy="false"
-#			deploy="true"
-#		else
-#			if["${TRAVIS_BRANCH}" = "master"]
-#			then
-#				export sfusername="${pkgusername}";
-#				export sfpassword="${pkgusername}";
-#				undeploy="false"
-#				deploy="true"
-#			else
-#				exit 1;
-#			fi
-#		fi
-#	fi
 if [ "${undeploy}" = "true" ]; then
   ant undeploy
 fi
